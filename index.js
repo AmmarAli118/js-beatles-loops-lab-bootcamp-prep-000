caf7a306e078f851269e654270fa81d565c2fe33
Ammar Ali <funnyguy118@gmail.com>
2019-01-10 04:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function theBeatlesPlay (musicians , instruments) {
 }
 
 function johnLennonFacts (array) {
+  
   var i = array.length
+  
   while (i > 0) {
     array[i] = array[i] + "!!!";
     i--;
